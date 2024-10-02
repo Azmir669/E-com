7e24017195abf207394542012a71b0f17dd0d069
Azmir669 <azmirvali5@gmail.com>
2024-10-02 21:44:23 +0530

    first commit

---
+++ backend-zip/src/config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+
+const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority'
+const connectDb=()=>{
+    return mongoose.connect(mongoDbUrl)
+}
+
+module.exports={connectDb}
